0ac5bc2743df4ed4eb68cedeb00c5c8e56c5ac2c
Rune Hartvig <rune_hartvig@hotmail.com>
2022-07-20 14:07:52 +0200

    finish implement use of score enum

--- src/app/core/models/score.enum.ts
+++ src/app/core/models/score.enum.ts
@@ -1,2 +1,11 @@
 export enum Score {
+    straightFlush = 8,
+    fourOfAKind = 7,
+    fullHouse = 6,
+    flush = 5,
+    straight = 4,
+    threeOfAKind = 3,
+    twoPair = 2,
+    onePair = 1,
+    highCard = 0
 }
--- src/app/core/services/hand-rank.service.ts
+++ src/app/core/services/hand-rank.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Card } from 'src/app/core/models/card';
+
+import { Card } from 'src/app/core/models/card.model';
 import { FlushService } from './flush.service';
 import { KindService } from './kind.service';
 import { StraightService } from './straight.service';
+import { Score } from '../models/score.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -20,32 +22,32 @@ export class HandRankService {
     
     // straigth flush
     if (this.straight.isStraight(hand) && this.flush.isFlush(hand)) {
-      return [8, Math.max(...ranks)]
+      return [Score.straightFlush, Math.max(...ranks)]
     }
 
     // four of a kind
     if (this.kind.kind(4, ranks)) {
-      return [7, this.kind.kind(4, ranks), this.kind.kind(1, ranks)]
+      return [Score.fourOfAKind, this.kind.kind(4, ranks), this.kind.kind(1, ranks)]
     }
 
     // full house
     if (this.kind.kind(3, ranks) && this.kind.kind(2, ranks)) {
-      return [6, this.kind.kind(3, ranks), this.kind.kind(2, ranks)]
+      return [Score.fullHouse, this.kind.kind(3, ranks), this.kind.kind(2, ranks)]
     }
 
     // flush
     if (this.flush.isFlush(hand)) {
-      return [5, this.sortDesc(ranks)]
+      return [Score.flush, this.sortDesc(ranks)]
     }
 
     // straight
     if (this.straight.isStraight(hand)) {
-      return [4, Math.max(...ranks)]
+      return [Score.straight, Math.max(...ranks)]
     }
 
     // three of a kind
     if (this.kind.kind(3, ranks)) {
-      return [3, this.kind.kind(3, ranks), this.sortDesc(ranks)]
+      return [Score.threeOfAKind, this.kind.kind(3, ranks), this.sortDesc(ranks)]
     }
 
     // two pair
@@ -53,11 +55,11 @@ export class HandRankService {
 
     // One pair
     if (this.kind.kind(2, ranks)) {
-      return [1, this.kind.kind(2, ranks), this.sortDesc(ranks)]
+      return [Score.onePair, this.kind.kind(2, ranks), this.sortDesc(ranks)]
     }
 
     // highest card
-    return [0, this.sortDesc(ranks)]
+    return [Score.highCard, this.sortDesc(ranks)]
   }
 
   sortDesc(ranks: number[]): number[] {
