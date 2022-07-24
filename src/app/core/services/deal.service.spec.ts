d2347c24232142b12a3d13cf300d81c5fa591403
Rune Hartvig <rune_hartvig@hotmail.com>
2022-07-24 10:24:01 +0200

    deal service logic and initial test

--- src/app/core/services/deal.service.spec.ts
+++ src/app/core/services/deal.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Suit } from '../models/suit.enum';
 
 import { DealService } from './deal.service';
 
@@ -13,4 +14,17 @@ describe('DealService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create a deck of cards', () => {
+    console.log(Object.values(Suit));
+    const deck = service.deal(4, 13);
+    console.log(deck[0][0])
+    expect(deck.length).toBe(4);
+    for (let i = 0; i < 4; i++) {
+      expect(deck[i].length).toBe(13);
+    }
+    expect(deck[0][0].rank).toBe(2);
+    expect(deck[0][0].suit).toBe(Suit.Clubs);
+  });
+
 });
--- src/app/core/services/deal.service.ts
+++ src/app/core/services/deal.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Card } from '../models/card.model';
+import { Rank } from '../models/rank.enum';
+import { Suit } from '../models/suit.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DealService {
 
-  constructor() { }
+  deck: Card[] = []
+
+  constructor() {
+
+    (Object.values(Suit)).filter(suit => typeof suit === 'number').forEach(suit => {
+      Object.values(Rank).filter(rank => typeof rank === 'number').forEach(rank => {
+          this.deck.push({rank: rank as Rank, suit: suit as Suit});
+      });
+    });
+  }
+
+  deal(numHands: number, cards=5, deck=this.deck) {
+    const hands = [];
+    for (let i = 0; i < numHands; i++) {
+      hands.push(deck.splice(0, cards));
+    }
+    return hands;
+  }
+
+  shuffle() {
+    for (let i = this.deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+    }
+  }
 }
