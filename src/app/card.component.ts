3d47431ff8596057042cb399ab6af5fa33b0ae47
Rune Hartvig <rune_hartvig@hotmail.com>
2022-07-26 21:32:12 +0200

    card color determined by suit

--- src/app/card.component.ts
+++ src/app/card.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+/**
+ * Card component
+ * a card component with a simple API:
+ * - `card.rank`: the card rank, a number between 2 and 14;
+ * - `card.suit`: the card suit, a string of 'spades', 'hearts', 'diamonds' or 'clubs';
+ */
+
 @Component({
   standalone: true,
   selector: 'app-card',
@@ -9,7 +16,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input() rank: number = 9;
-  @Input() suit : string = 'clubs';
+  @Input() suit : string = 'hearts';
+
+  suitColor = this.suit === 'spades' || this.suit === 'clubs' ? 'black' : 'red';
 
   suitMap: {[key: string]: string} = {
     clubs: '♣',
