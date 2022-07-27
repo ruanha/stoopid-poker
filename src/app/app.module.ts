a437689baf321f1b87613a0992061c229eeb0380
Rune Hartvig <rune_hartvig@hotmail.com>
2022-07-27 20:52:04 +0200

    init hand component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card.component';
+import { HandComponent } from './hand/hand.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HandComponent,
   ],
   imports: [
     BrowserModule,
