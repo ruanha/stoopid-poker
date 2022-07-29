82e6d4f06de4faea81eba27d516494d289785cad
Rune Hartvig <rune_hartvig@hotmail.com>
2022-07-29 18:07:50 +0200

    introduce game mechanic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Card } from './core/models/card.model';
 import { DealService } from './core/services/deal.service';
+import { PokerService } from './core/services/poker.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,45 @@ import { DealService } from './core/services/deal.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public dealService: DealService) {}
+  constructor(
+    public dealService: DealService,
+    public pokerService: PokerService
+  ) {}
   title = 'poker';
-  hands = this.dealService.deal(2, 5);
+  hands: Card[][] = [];
+  playerHand: Card[] = [];
+  opponentHand: Card[] = [];
+  dealt = false;
+  bet: string = '';
+  result: string = '';
+  score = {you: 0, opponent: 0};
+
+  deal() {
+    this.hands = this.dealService.deal(2, 5);
+    this.playerHand = this.hands[0];
+    this.opponentHand = this.hands[1];
+    this.dealt = true;
+  }
+
+  action(action: string): void {
+    this.bet = action;
+    const winners = this.pokerService.poker(this.hands);
+    
+    if (winners.length > 1) {
+      this.result = 'tie';
+    }
+    if (winners[0] === this.playerHand) {
+      this.result = 'win';
+    }
+    if (winners[0] === this.opponentHand) {
+      this.result = 'lose';
+    }
+    if (this.result === this.bet) {
+      this.score.you += 1;
+    } else {
+      this.score.opponent += 1;
+    }
+    this.dealt = false;
+  }
+
 }
