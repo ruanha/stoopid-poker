c93741277110016f5f25f403c7bfe5dad4bde2cb
Rune Hartvig <rune_hartvig@hotmail.com>
2022-08-01 13:21:36 +0200

    overlay named hand over cards

--- src/app/hand/hand.component.ts
+++ src/app/hand/hand.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../core/models/card.model';
 import { CardTrigonometryService } from '../core/services/display/card-trigonometry.service';
+import { NamedHandService } from '../core/services/named-hand.service';
 import { RankToCharService } from '../core/services/rank-to-char.service';
 import { SuitToStringService } from '../core/services/suit-to-string.service';
 
@@ -13,11 +14,13 @@ export class HandComponent implements OnInit {
 
   @Input() hand!: Card[];
   @Input() backside = false;
+  @Input() showHandName: boolean = false;
 
   constructor(
     public suitToStringService: SuitToStringService,
     public rankToCharService: RankToCharService,
-    public cardTrigonometryService: CardTrigonometryService) { }
+    public cardTrigonometryService: CardTrigonometryService,
+    public namedHandService: NamedHandService) { }
 
 
   ngOnInit(): void {
